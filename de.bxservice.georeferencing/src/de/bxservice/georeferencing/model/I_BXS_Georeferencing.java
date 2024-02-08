3205cb0a95c20a2a3957d9d523cea01d13b73aac
idempiere-consulting <info@idempiere.consulting>
2024-02-08 12:48:10 +0100

    aChecchia: plug-in georeferencing --- update code for progress core_iDempiere 11.0_Synergy

--- de.bxservice.georeferencing/src/de/bxservice/georeferencing/model/I_BXS_Georeferencing.java
+++ de.bxservice.georeferencing/src/de/bxservice/georeferencing/model/I_BXS_Georeferencing.java
@@ -18,7 +18,8 @@ package de.bxservice.georeferencing.model;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
-import org.compiere.model.*;
+
+import org.compiere.model.MTable;
 import org.compiere.util.KeyNamePair;
 
 /** Generated Interface for BXS_Georeferencing
--- de.bxservice.georeferencing/src/de/bxservice/georeferencing/model/X_BXS_Georeferencing.java
+++ de.bxservice.georeferencing/src/de/bxservice/georeferencing/model/X_BXS_Georeferencing.java
@@ -20,7 +20,11 @@ package de.bxservice.georeferencing.model;
 import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.util.Properties;
-import org.compiere.model.*;
+
+import org.compiere.model.I_Persistent;
+import org.compiere.model.MTable;
+import org.compiere.model.PO;
+import org.compiere.model.POInfo;
 import org.compiere.util.Env;
 import org.compiere.util.KeyNamePair;
 
